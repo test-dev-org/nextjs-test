6c7f350c488ccb887bfe6170e993bf9ffd67d404
devjiwonchoi <devjiwonchoi@gmail.com>
2025-06-19 14:55:56 +0200

    add indicatorPosition to story

--- packages/next/src/next-devtools/dev-overlay/components/errors/dev-tools-indicator/dev-tools-indicator.stories.tsx
+++ packages/next/src/next-devtools/dev-overlay/components/errors/dev-tools-indicator/dev-tools-indicator.stories.tsx
@@ -61,6 +61,7 @@ const state: OverlayState = {
   isErrorOverlayOpen: false,
   // TODO: This will be handled on the next stack——with proper story.
   isDevToolsPanelOpen: false,
+  indicatorPosition: 'bottom-left',
 }
 
 export const StaticRoute: Story = {
