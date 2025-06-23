b0b4394ff85f0db290ade5809a4381864b404a97
Jiachi Liu <inbox@huozhi.im>
2025-06-23 11:56:46 +0200

    remove links

--- test/development/app-dir/segment-explorer/app/parallel-routes-edge/no-bar/page.tsx
+++ test/development/app-dir/segment-explorer/app/parallel-routes-edge/no-bar/page.tsx
@@ -1,10 +1,7 @@
-import Link from 'next/link'
-
 export default function Page() {
   return (
     <div>
       <p>no bar</p>
-      <Link href="/parallel-routes-edge">Back to /parallel-routes-edge</Link>
     </div>
   )
 }
