6e88a38f897be47808b9c983e956488f754461a1
Hendrik Liebau <mail@hendrik-liebau.de>
2025-06-24 22:56:32 +0200

    Revert change in next config

--- test/e2e/app-dir/parallel-routes-and-interception/next.config.js
+++ test/e2e/app-dir/parallel-routes-and-interception/next.config.js
@@ -2,9 +2,6 @@
  * @type {import('next').NextConfig}
  */
 const nextConfig = {
-  experimental: {
-    ppr: process.env.__NEXT_EXPERIMENTAL_PPR === 'true',
-  },
   async rewrites() {
     return {
       beforeFiles: [
